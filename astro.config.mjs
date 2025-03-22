2f625157667e115a742872115a913cb59110d723
Jsinco <jonah@jsinco.dev>
2025-03-22 13:01:40 -0400

    Start adding guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwindcss from "@tailwindcss/vite";
+
+const starlightConfig = {
+  logo: { src: "@/assets/luma-logo.webp" },
+  title: "Wiki",
+  customCss: ["./src/styles/custom.css"],
+  social: {
+    github: "https://github.com/withastro/starlight",
+  },
+  sidebar: [
+    // {
+    //   label: "Guides",
+    //   items: [
+    //     // Each item here is one entry in the navigation menu.
+    //     { label: "Example Guide", slug: "guides/example" },
+    //   ],
+    // },
+    // {
+    //   label: "Reference",
+    //   autogenerate: { directory: "reference" },
+    // },
+    {
+      label: "Information",
+      autogenerate: { directory: "information" },
+    },
+    {
+      label: "Crossplay",
+      autogenerate: { directory: "crossplay" },
+    },
+    {
+      label: "Basics",
+      autogenerate: { directory: "basics" },
+    },
+    {
+      label: "Features",
+      autogenerate: { directory: "features" },
+    },
+  ],
+};
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight(starlightConfig)],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
